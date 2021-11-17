a88450a03fc0c570ae05fe8b4a9a72e2095c403c
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-17 14:43:57 +0100

    Initial commit

---
+++ src/org/generation/italy/array/cibiPreferiti.java
@@ -0,0 +1,12 @@
+package org.generation.italy.array;
+
+public class cibiPreferiti {
+
+	public static void main(String[] args) {
+		String[] cibi = {"carbonara","amatrician","fiorentina","tiramisu","burrata","mortadella"};
+		int x=cibi.length;
+		System.out.println("al primo posto troviami: "+cibi[0]);
+		System.out.println("all'ultimo posto abbiamo: "+cibi[x-1]);
+	}
+
+}
